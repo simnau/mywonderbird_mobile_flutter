3ae7f0647d6558a4a0c0330bc6ef00323ef3d2bd
Simonas Naulickas <simonas.naulickas@gmail.com>
2021-11-15 18:37:47 +0200

    Switch to using the native image picker when sharing photos

--- lib/routes/picture-sharing/pages/select-upload-type/main.dart
+++ lib/routes/picture-sharing/pages/select-upload-type/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mywonderbird/components/typography/subtitle1.dart';
 import 'package:mywonderbird/constants/theme.dart';
 import 'package:mywonderbird/locator.dart';
 import 'package:mywonderbird/services/navigation.dart';
 import 'package:mywonderbird/services/picture-data.dart';
+import 'package:mywonderbird/types/picture-data.dart';
+import 'package:mywonderbird/util/snackbar.dart';
 
-import '../select-picture/main.dart';
 import '../share-pictures-trip/main.dart';
 import '../share-pictures-standalone/main.dart';
 import '../../components/big-action-button.dart';
 
-class SelectUploadType extends StatelessWidget {
+class SelectUploadType extends StatefulWidget {
   final List<String> imagePaths;
 
   const SelectUploadType({
     Key key,
     this.imagePaths,
   }) : super(key: key);
+
+  @override
+  State<SelectUploadType> createState() => _SelectUploadTypeState();
+}
+
+class _SelectUploadTypeState extends State<SelectUploadType> {
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +40,12 @@ class SelectUploadType extends StatelessWidget {
   }
 
   Widget _body(BuildContext context) {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
     return Padding(
       padding: EdgeInsets.all(spacingFactor(2)),
       child: Column(
@@ -57,48 +72,55 @@ class SelectUploadType extends StatelessWidget {
   }
 
   _onCreateTrip() async {
-    final navigationService = locator<NavigationService>();
-
-    if (imagePaths != null && imagePaths.isNotEmpty) {
-      final pictureDataService = locator<PictureDataService>();
-
-      final pictureDatas = await pictureDataService.extractPicturesData(
-        imagePaths,
-        false,
-      );
-
-      navigationService.push(
-        MaterialPageRoute(
-          builder: (_) => SharePicturesTrip(pictureDatas: pictureDatas),
-        ),
-      );
-    } else {
-      navigationService.push(MaterialPageRoute(
-        builder: (context) => SelectPicture(isStandalone: false),
-      ));
-    }
+    _onSelectType(false);
   }
 
   _onSingleLocation() async {
+    _onSelectType(true);
+  }
+
+  _onSelectType(bool isSingle) async {
     final navigationService = locator<NavigationService>();
+    final pictureDataService = locator<PictureDataService>();
+    List<PictureData> pictureDatas;
 
-    if (imagePaths != null && imagePaths.isNotEmpty) {
-      final pictureDataService = locator<PictureDataService>();
+    setState(() {
+      _isLoading = true;
+    });
 
-      final pictureDatas = await pictureDataService.extractPicturesData(
-        imagePaths,
-        true,
-      );
+    try {
+      if (widget.imagePaths != null && widget.imagePaths.isNotEmpty) {
+        pictureDatas = await pictureDataService.extractPicturesData(
+          widget.imagePaths,
+          isSingle,
+        );
+      } else {
+        final images = await ImagePicker().pickMultiImage();
+        final paths = images.map((image) => image.path).toList();
+
+        pictureDatas = await pictureDataService.extractPicturesData(
+          paths,
+          isSingle,
+        );
+      }
 
       navigationService.push(
         MaterialPageRoute(
-          builder: (_) => SharePicturesStandalone(pictureDatas: pictureDatas),
+          builder: (_) => isSingle
+              ? SharePicturesStandalone(pictureDatas: pictureDatas)
+              : SharePicturesTrip(pictureDatas: pictureDatas),
         ),
       );
-    } else {
-      navigationService.push(MaterialPageRoute(
-        builder: (context) => SelectPicture(isStandalone: true),
-      ));
+    } catch (e) {
+      final snackBar = createErrorSnackbar(
+        text: "There was an error selecting pictures. Please try again",
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 }
